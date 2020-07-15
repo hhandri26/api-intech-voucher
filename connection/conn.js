42b48064eef200ade4ce99b51fc32bb8eccc819c
unknown <handrisaeputra@gmail.com>
2020-07-15 21:32:29 +0700

    go online 3

--- connection/conn.js
+++ connection/conn.js
@@ -1,19 +1,19 @@
 var mysql = require('mysql');
 
-// var con = mysql.createConnection({
-//   host: "103.136.76.250",
-//   user: "hhandri26",
-//   password: "Cilangkap126",
-//   database: "my-voucher"
-// });
-
-
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
+  host: "103.136.76.250",
+  user: "hhandri26",
+  password: "Cilangkap126",
   database: "my-voucher"
 });
+
+
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "my-voucher"
+// });
 
 
 con.connect(function (err){
